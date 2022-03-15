b9643153f01b9b663cbf14c7f89a353dac9b800b
looph0le <mitanshpanchal@gmail.com>
2022-03-15 14:50:13 +0530

    all

---
+++ helloJava.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+   import java.applet.*;
+
+   class helloJava extends Applet{
+       public void paint(Graphics g){
+           g.drawString("Hello Java", 10, 100);
+       }
+   }
